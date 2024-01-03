092a5d2915fb514fca2882fa2ab3d45d201c73a0
karolisreimeris <karolisreim@gmail.com>
2024-01-03 12:05:04 +0200

    Change jetstream tests path, update database seeder, update test database name

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,11 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +20,14 @@ class DatabaseSeeder extends Seeder
         $posts = Post::factory(150)->recycle($users)->create();
 
         Comment::factory(75)->recycle($users)->recycle($posts)->create();
+
+        User::factory()
+            ->has(Post::factory(30))
+            ->has(Comment::factory(90)->recycle($posts))
+            ->create([
+                'name' => 'Karolis',
+                'email' => 'karolis@example.com',
+                'password' => Hash::make('password'),
+            ]);
     }
 }
