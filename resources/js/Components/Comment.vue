1dbdf28049a357ae85750ad2a42ef2ce36bae663
karolisreimeris <karolisreim@gmail.com>
2024-01-15 16:31:04 +0200

    Redirect after delete comment

--- resources/js/Components/Comment.vue
+++ resources/js/Components/Comment.vue
@@ -3,12 +3,14 @@
         <div class="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
             <img :src="comment.user.profile_photo_url" class="h-10 w-10 rounded-full" alt=""/>
         </div>
-        <div>
+        <div class="flex-1">
             <p class="mt-1 break-all">{{ comment.body }}</p>
-            <span class="first-letter:uppercase block pt-1 text-xs text-gray-600">By {{ comment.user.name }} {{ relativeDate(comment.created_at) }} ago</span>
-            <div class="mt-1" v-if="comment.can?.delete">
-                <form @submit.prevent="deleteComment">
-                    <button>Delete</button>
+            <span class="first-letter:uppercase block pt-1 text-xs text-gray-600">
+                By {{ comment.user.name }} {{ relativeDate(comment.created_at) }} ago
+            </span>
+            <div class="mt-1 text-right empty:hidden" v-if="comment.can?.delete">
+                <form @submit.prevent="$emit('delete', comment.id)">
+                    <button class="text-xs text-red-700 hover:font-semibold">Delete</button>
                 </form>
             </div>
         </div>
@@ -17,11 +19,8 @@
 
 <script setup>
 import {relativeDate} from "@/Utilities/date.js";
-import {router} from "@inertiajs/vue3";
 
 const props = defineProps(['comment']);
 
-const deleteComment = () => router.delete(route('comments.destroy', props.comment.id), {
-    preserveScroll: true,
-});
+const emit = defineEmits(['delete']);
 </script>
